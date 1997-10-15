d22917fac82ac54cbbe5a69f4f40501032e725d3
john <john>
1997-10-15 22:19:14 +0000

    (rm-burst-message, rm-really-burst-message): now works properly, and handles both RFC-934 _and_ RFC-1153 encapsulation schemes; seems to work ok..

--- lisp/rm-misc.jl
+++ lisp/rm-misc.jl
@@ -166,23 +166,25 @@ it to. When ALL-HEADERS-P is non-nil non-visible headers will be included."
 
 ;; Message bursting
 
-;;;###autoload
-(defun rm-burst-message ()
-  "Burst the currently displayed message, inserting the messages at the end
-of the folder. The message being exploded should conform to the RFC-934
-standard for encapsulating messages. Some effort will be made to avoid
-inserting rubbish. The original message will be marked as deleted."
-  (interactive)
+(defvar rm-rfc1153-preamble-sep (concat ?^ (make-string 70 ?-) ?$))
+(defvar rm-rfc1153-message-sep (concat ?^ (make-string 30 ?-) ?$))
+(defvar rm-rfc1153-stuffed-re (concat "^ (" (make-string 30 ?-) ")$"))
+
+(defvar rm-rfc934-preamble-sep "^-[^ ]")
+(defvar rm-rfc934-message-sep rm-rfc934-preamble-sep)
+(defvar rm-rfc934-stuffed-re "^- (.*)$")
+
+(defun rm-really-burst-message (preamble-sep message-sep stuffed-re)
   (let
       ((inhibit-read-only t)
-       (input-start rm-current-msg-body)
-       ;; Work backwards from the last message in the digest
-       (input-pos rm-current-msg-end)
+       (input-pos rm-current-msg-body)
+       (input-end rm-current-msg-end)
        (last-pos nil)
        (original-msg rm-current-msg)
        (msgs nil)
        (count 0)
-       output-pos new-msg)
+       output-pos
+       new-msg)
     (unrestrict-buffer)
     (let
 	((rm-move-after-deleting nil))
@@ -190,56 +192,90 @@ inserting rubbish. The original message will be marked as deleted."
     ;; Find the last message in the folder
     (while rm-after-msg-list
       (rm-move-forwards))
-    (restrict-buffer input-start input-pos)
-    (unless (setq input-pos (find-prev-regexp "^-[^ ]" input-pos))
-      (error "Not an RFC-934 digest!"))
-    (setq last-pos input-pos
-	  input-pos (prev-line 1 (copy-pos input-pos)))
-    (while (setq input-pos (find-prev-regexp "^-[^ ]" input-pos))
+
+    ;; Find the start of the first message
+    (restrict-buffer input-pos input-end)
+    (unless (setq input-pos (find-next-regexp preamble-sep input-pos))
+      (error "Can't find digest preamble!"))
+    (setq input-pos (next-line 1 input-pos)
+	  last-pos input-pos)
+    (while (setq input-pos (find-next-regexp message-sep input-pos))
       (unrestrict-buffer)
       (let
-	  ((start (next-line 1 (copy-pos input-pos))))
+	  ((start last-pos))
 	(when (looking-at "([\t ]*\n)+" start)
 	  (setq start (match-end)))
-	;; Try to ignore junk!?
-	(if (not (or (looking-at "^>?From " start)
-		     (looking-at mail-header-name start)))
-	    (message "Ignoring nonsense message!")
-	  ;; Enforce the "\n\n" rule between messages
-	  (rm-enforce-msg-separator)
-	  (setq output-pos (cursor-pos))
-	  (insert (copy-area start last-pos))
-	  ;; Unmangle "^- " lines
-	  (let
-	      ((pos (cursor-pos)))
-	    (restrict-buffer output-pos pos)
-	    (while (setq pos (find-prev-regexp "^- " pos))
-	      (delete-area (match-end) (match-start))
-	      (setq pos (prev-line 1 pos)))
-	    (unrestrict-buffer))
-	  ;; Unmangle quoted ^From_
-	  (when (looking-at "^>From " output-pos)
-	    (delete-area output-pos (next-char 1 (copy-pos output-pos))))
-	  (unless (looking-at mail-message-start output-pos)
-	    ;; No ^From_ line, kludge one ourselves
-	    (insert (concat "From jade " (current-time-string) ?\n)
-		    output-pos))
-	  (when (setq new-msg (rm-build-message-struct output-pos))
-	    (setq count (1+ count)
-		  msgs (cons new-msg msgs))
-	    (rm-set-flag new-msg 'unread)))
-	(setq last-pos input-pos
-	      input-pos (prev-line 1 (copy-pos input-pos)))
-	(restrict-buffer input-start last-pos)))
+	;; Ignore null message
+	(unless (looking-at message-sep start)
+	  ;; Try to ignore junk!?
+	  (if (not (or (looking-at "^>?From " start)
+		       (looking-at mail-header-name start)))
+	      (message "Ignoring nonsense message!")
+	    ;; Enforce the "\n\n" rule between messages
+	    (goto-buffer-end)
+	    (rm-enforce-msg-separator)
+	    (setq output-pos (cursor-pos))
+	    (insert (copy-area start input-pos))
+	    ;; Unmangle stuffed lines
+	    (let
+		((pos (cursor-pos)))
+	      (restrict-buffer output-pos pos)
+	      (while (setq pos (find-prev-regexp stuffed-re pos))
+		(replace-regexp stuffed-re "\\1" pos)
+		(setq pos (prev-line 1 pos)))
+	      (unrestrict-buffer))
+	    ;; Unmangle quoted ^From_
+	    (when (looking-at "^>From " output-pos)
+	      (delete-area output-pos (next-char 1 (copy-pos output-pos))))
+	    (unless (looking-at mail-message-start output-pos)
+	      ;; No ^From_ line, kludge one ourselves
+	      (insert (concat "From jade " (current-time-string) ?\n)
+		      output-pos))
+	    (when (setq new-msg (rm-build-message-struct output-pos))
+	      (setq count (1+ count)
+		    msgs (cons new-msg msgs))
+	      (rm-set-flag new-msg 'unread))))
+	(setq input-pos (next-line 1 input-pos)
+	      last-pos input-pos)
+	(restrict-buffer input-pos input-end)))
     (unrestrict-buffer)
-    (when rm-current-msg
-      (setq rm-before-msg-list (cons rm-current-msg
-				     rm-before-msg-list)
-	    rm-current-msg-index (1+ rm-current-msg-index)))
-    (setq rm-current-msg (car msgs)
-	  rm-after-msg-list (cdr msgs)
-	  rm-message-count (+ rm-message-count count)
-	  rm-cached-msg-list 'invalid)
+    (when msgs
+      (when rm-current-msg
+	(setq rm-before-msg-list (cons rm-current-msg
+				       rm-before-msg-list)
+	      rm-current-msg-index (1+ rm-current-msg-index)))
+      (setq msgs (nreverse msgs)
+	    rm-current-msg (car msgs)
+	    rm-after-msg-list (cdr msgs)
+	    rm-message-count (+ rm-message-count count)
+	    rm-cached-msg-list 'invalid))
     (rm-with-summary
      (summary-update))
     (rm-display-current-message)))
+  
+;;;###autoload
+(defun rm-burst-message ()
+  "Burst the currently displayed message, inserting the messages at the end
+of the folder. Prompts for the digest type, RFC-934 or RFC-1153."
+  (interactive)
+  (let
+      ((tem (find-next-regexp rm-rfc934-preamble-sep (buffer-start))))
+    (when tem
+      (if (looking-at rm-rfc1153-preamble-sep tem)
+	  (setq tem 'rfc1153)
+	(setq tem 'rfc934)))
+    ;; Now tem has the guessed default type or nil
+    (setq tem (intern (prompt-from-list '("rfc934" "rfc1153")
+					"Type of digest (`rfc934' or `rfc1153'):"
+					(and tem (symbol-name tem)))))
+    (cond
+     ((eq tem 'rfc934)
+      (rm-really-burst-message rm-rfc934-preamble-sep
+			       rm-rfc934-message-sep
+			       rm-rfc934-stuffed-re))
+     ((eq tem 'rfc1153)
+      (rm-really-burst-message rm-rfc1153-preamble-sep
+			       rm-rfc1153-message-sep
+			       rm-rfc1153-stuffed-re))
+     (t
+      (error "Unknown digest type" tem)))))
