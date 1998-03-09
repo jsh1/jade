14590cd5eaba5b0f76fc4370f1019aaafbea7107
john <john>
1998-03-09 09:37:11 +0000

    (mail-month-alist): alist of RFC-822 month abbrevs to numeric indices (Jan=1, ...)

--- lisp/maildefs.jl
+++ lisp/maildefs.jl
@@ -141,130 +141,13 @@ as a single blank line or the start of the buffer, followed by this regexp.")
   "Regular expression defining a single atom in a mail header. May not
 include any parenthesised expressions!")
 
+(defvar mail-month-alist '(("Jan" . 1) ("Feb" . 2) ("Mar" . 3) ("Apr" . 4)
+			   ("May" . 5) ("Jun" . 6) ("Jul" . 7) ("Aug" . 8)
+			   ("Sep" . 9) ("Oct" . 10) ("Nov" . 11) ("Dec" . 12))
+   "Alist of (MONTH-NAME . MONTH-NUM).")
+
 
-;;;; Mail utility functions
-
-;; Return the start of the header following the header starting at POS
-;; This returns nil to show that the header goes to the end of the
-;; buffer or restriction
-(defun mail-unfold-header (pos)
-  (if (looking-at ".*\n([\t ].*\n)*" pos)
-      (match-end)
-    nil))
-
-;; Return (START . END) of the atom-ish expression at POS. "atom-ish"
-;; includes comments, strings, angle-delimited addresses, and groups of
-;; normal atoms, importantly though, _no_ commas
-(defun mail-parse-atom (pos &aux char)
-  (when (looking-at "[\t\n ]+" pos)
-    (setq pos (match-end)))
-  (when (setq char (get-char pos))
-    (cond
-     ((member char '(?\  ?\t ?\n))
-      ;; Whitespace
-      (looking-at "[\t\n ]+" pos)
-      (cons pos (match-end)))
-     ((= char ?\")
-      ;; A string
-      (unless (re-search-forward "[^\\]\"" (forward-char 1 pos))
-	(error "Unterminated string in list, %s" pos))
-      (cons pos (match-end)))
-     ((= char ?\()
-      ;; A comment
-      (unless (re-search-forward "[^\\]\\)" (forward-char 1 pos))
-	(error "Unterminated comment in list, %s" pos))
-      (cons pos (match-end)))
-     ((= char ?\<)
-      ;; An address spec
-      (unless (re-search-forward "[^\\]>" (forward-char 1 pos))
-	(error "Unterminated address in list, %s" pos))
-      (cons pos (match-end)))
-     ((= char ?,)
-      ;; A comma
-      nil)
-     (t
-      ;; Some sort of atom
-      (unless (looking-at (concat ?\( mail-atom-re
-				      "|[][.:;@]|[\t\n ]+\)+") pos)
-	(error "Can't parse atom, %s" pos))
-      (cons pos (match-end))))))
-
-;; Parse one list of atoms, ended by a comma or EOF. Returns (STRING . END),
-;; the text of the group of atoms, and the position of the first non-included
-;; character
-(defun mail-parse-group (pos)
-  (let
-      ((list '())
-       tem)
-    (while (setq tem (mail-parse-atom pos))
-      (setq list (cons (copy-area (car tem) (cdr tem)) list))
-      (setq pos (cdr tem)))
-    (when list
-      (cons (nreverse list) pos))))
-
-;; Parse a list of comma-separated mail addresses, returns a list of
-;; strings. Stops parsing at the end of the header starting at POS.
-;; NO-COMMA-SEPS controls whether a comma-separated list is parsed,
-;; or simply a sequence of "groups" (from the above function)
-(defun mail-parse-list (pos &optional no-comma-seps)
-  (save-restriction
-    ;; Restrict ourselves to the current header
-    (restrict-buffer pos (forward-char -1 (or (mail-unfold-header pos)
-					      (end-of-buffer))))
-    (when (looking-at (concat mail-header-name "[\t ]*") pos)
-      (setq pos (match-end)))
-    (if no-comma-seps
-	(car (mail-parse-group pos))
-      (let
-	  (list tem)
-	(while (setq tem (mail-parse-group pos))
-	  (setq list (cons (apply 'concat (car tem)) list)
-		pos (if (looking-at "[\t\n ]*,[\t\n ]*" (cdr tem))
-			(match-end)
-		      (cdr tem))))
-	(nreverse list)))))
-
-;; Return the position at which a header matching HEADER occurs, or
-;; nil if one doesn't
-(defun mail-find-header (header &optional pos)
-  (when (and (re-search-forward (concat "^(" header "[\t ]*:[\t ]*|$)")
-			       (or pos (start-of-buffer)) nil t)
-	     (> (match-end) (match-start)))
-    (match-end)))
-
-;; Return a copy of the header named HEADER in the current buffer. This
-;; will be a string with newlines converted to spaces, unless LISTP is
-;; non-nil in which case the header will be split into a list of items
-;; (separated by commas, unless NOT-COMMA-SEPARATED is t).
-(defun mail-get-header (header &optional listp not-comma-separated)
-  (let
-      ((pos (mail-find-header header)))
-    (when pos
-      (if listp
-	  (let
-	      ((list (mail-parse-list pos not-comma-separated)))
-	    (while (setq pos (mail-find-header header
-					       (mail-unfold-header pos)))
-	      (setq list (nconc list (mail-parse-list pos
-						      not-comma-separated))))
-	    list)
-	(translate-string (copy-area pos (or (mail-unfold-header pos)
-					     (end-of-buffer)))
-			  flatten-table)))))
-
-;; Delete the header at POS and return the position of the following
-;; header, or nil for when the end of the buffer/restriction is reached
-(defun mail-delete-header (pos)
-  (let
-      ((end (mail-unfold-header pos)))
-    (if end
-	(progn
-	  (delete-area pos end)
-	  pos)
-      ;; Header goes up to the end of the restriction, delete the
-      ;; previous newline instead of the next
-      (delete-area (forward-char -1 pos) (end-of-buffer))
-      nil)))
+;; A couple of utility functions
 
 ;; Return a list of inboxes for the mail folder FOLDER
 (defun mail-find-inboxes (folder)
@@ -291,40 +174,11 @@ include any parenthesised expressions!")
 	(setq tem (cdr tem)))
       list))))
 
-;; Insert a list of comma separated items. Breaks the list to satisfy
-;; mail-fill-column. Unless NO-COMMAS is t, each item is separated by
-;; a comma
-(defun mail-insert-list (list &optional no-commas)
-  (let
-      ((initial-indent (pos-col (char-to-glyph-pos))))
-    (while list
-      (when (> (+ (length (car list))
-		  (pos-col (char-to-glyph-pos (cursor-pos))))
-	     mail-fill-column)
-      (insert "\n")
-      (indent-to initial-indent))
-      (insert (car list))
-      (when (and (not no-commas) (cdr list))
-	(insert ", "))
-      (setq list (cdr list)))))
-
-;; Return a string constructed from address ADDR and name NAME, according
-;; to mail-address-style
-(defun mail-format-address (addr name)
-  ;; Need to handle quoting full name a la RFC-822
-  (cond
-   ((eq mail-address-style 'angles)
-    (concat name " \<" addr "\>"))
-   ((eq mail-address-style 'parens)
-    (concat addr " \(" name "\)"))
-   (t
-    addr)))
-
 ;; History list of prompt-for-folder
 (defvar mail-prompt-history (make-ring))
 
 ;; Prompt for the name of a mail folder
 (defun prompt-for-folder (title &optional default)
   (prompt-for-file title nil
-		   (and default (file-name-directory default)) default
+		   (or default (file-name-directory default)) default
 		   mail-prompt-history))
