643d194205694878a91c1f02d17f10dcc5c86976
john <john>
1998-03-13 18:03:01 +0000

    use after-read-file-hook, not find-file-hook

--- lisp/rcs-hooks.jl
+++ lisp/rcs-hooks.jl
@@ -34,7 +34,7 @@
   (bind-keys ctrl-x-keymap
     "v" '(setq next-keymap-path '(rcs-keymap)))
   (setq rcs-hooks-initialised t)
-  (add-hook 'find-file-hook 'rcs-find-file-function))
+  (add-hook 'after-read-file-hook 'rcs-read-file-function))
 
 ;; Returns t if FILE-NAME is under RCS control
 (defun rcs-file-p (file-name)
@@ -43,7 +43,7 @@
 		      (file-name-directory file-name) "RCS"
 		      (concat (file-name-nondirectory file-name) ",v")))))
 
-;; Function called from the find-file-hook
-(defun rcs-find-file-function (buffer)
+;; Function called from the after-read-file-hook
+(defun rcs-read-file-function (buffer)
   (when (rcs-file-p (buffer-file-name buffer))
     (rcs-init-file buffer)))
