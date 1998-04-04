913984d57b3556ed7fe553ad72bd1c321c4e4f97
john <john>
1998-04-04 18:42:35 +0000

    require file-subst

--- lisp/html-style.jl
+++ lisp/html-style.jl
@@ -18,6 +18,7 @@
 ;;; along with Jade; see the file COPYING.  If not, write to
 ;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
+(require 'file-subst)
 (provide 'html-style)
 
 ;;; Commentary:
