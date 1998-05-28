6e04e5e83a961239792cc8d4439751a540cc3b2d
john <john>
1998-05-28 17:06:17 +0000

    (mail-month-alist): added full names, as well as abbreviations (mail-timezone-alist): moved from mail-headers.jl

--- lisp/maildefs.jl
+++ lisp/maildefs.jl
@@ -150,8 +150,20 @@ include any parenthesised expressions!")
 
 (defvar mail-month-alist '(("Jan" . 1) ("Feb" . 2) ("Mar" . 3) ("Apr" . 4)
 			   ("May" . 5) ("Jun" . 6) ("Jul" . 7) ("Aug" . 8)
-			   ("Sep" . 9) ("Oct" . 10) ("Nov" . 11) ("Dec" . 12))
+			   ("Sep" . 9) ("Oct" . 10) ("Nov" . 11) ("Dec" . 12)
+			   ("January" . 1) ("February" . 2) ("March" . 3)
+			   ("April" . 4) ("June" . 6) ("July" . 7)
+			   ("August" . 8) ("September" . 9) ("October" . 10)
+			   ("November" . 11) ("December" . 12))
    "Alist of (MONTH-NAME . MONTH-NUM).")
+
+(defvar mail-timezone-alist
+  '(("UT" . 0) ("GMT" . 0)
+    ("EST" . -300) ("EDT" . -240)
+    ("CST" . -360) ("CDT" . -300)
+    ("MST" . -420) ("MDT" . -360)
+    ("PST" . -480) ("PDT" . -420))
+  "Alist of (TIMEZONE . MINUTES-DIFFERENCE).")
 
 
 ;; A couple of utility functions
