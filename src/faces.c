4a9c155fea761ba43968dc6466e8e3f15059e9bb
john <john>
1998-09-02 16:54:48 +0000

    (cmd_make_face): no longers copies from a source face, now all faces are created equal (and empty)
    (cmd_delete_face, mark_faces, cmd_face_id): deleted, no longer needed
    now faces don't have id's associated with them
    
    (cmd_set_face_attribute, cmd_face_attribute): new attributes `inverted'
    and `boxed'
    
    (merge_faces): new function, compute a face for an extent
    
    (get_face_id): new function, compute a face from a face
    
    (mark_merged_faces): new function

--- src/faces.c
+++ src/faces.c
@@ -21,10 +21,13 @@
 #include "jade.h"
 #include <lib/jade_protos.h>
 #include <string.h>
+#include <assert.h>
 
 _PR void face_prin(VALUE strm, VALUE obj);
 _PR void face_sweep(void);
-_PR void mark_faces(void);
+_PR int merge_faces(WIN *w, Lisp_Extent *e, int in_active, int on_cursor);
+_PR int get_face_id(WIN *w, Lisp_Face *f);
+_PR void mark_merged_faces(WIN *w);
 _PR void color_prin(VALUE strm, VALUE obj);
 _PR void color_sweep(void);
 _PR void faces_init(void);
@@ -32,16 +35,18 @@ _PR void faces_init(void);
 
 /* faces */
 
-_PR Lisp_Face *allocated_faces, *face_table[];
+_PR Lisp_Face *allocated_faces;
 Lisp_Face *allocated_faces;
-Lisp_Face *face_table[GA_LastFace+1];
 
 DEFSYM(foreground, "foreground");
 DEFSYM(background, "background");
 DEFSYM(underline, "underline");
 DEFSYM(bold, "bold");
 DEFSYM(italic, "italic");
+DEFSYM(inverted, "inverted");
+DEFSYM(boxed, "boxed");
 _PR VALUE sym_foreground, sym_background, sym_underline, sym_bold, sym_italic;
+_PR VALUE sym_inverted, sym_boxed;
 
 DEFSYM(default_face, "default-face");
 DEFSYM(block_face, "block-face");
@@ -59,19 +64,17 @@ char *default_ml_color = "lightsteelblue";
 _PR char *default_fg_color, *default_bg_color;
 _PR char *default_block_color, *default_hl_color, *default_ml_color;
 
-_PR VALUE cmd_make_face(VALUE, VALUE);
-DEFUN("make-face", cmd_make_face, subr_make_face, (VALUE name, VALUE src),
-      V_Subr2, DOC_make_face) /*
+_PR VALUE cmd_make_face(VALUE);
+DEFUN("make-face", cmd_make_face, subr_make_face, (VALUE name),
+      V_Subr1, DOC_make_face) /*
 ::doc:make_face::
-make-face NAME [SOURCE]
+make-face NAME
 
 Return the face called NAME. If no face with this name exists already, create
-a new one, its initial state will be copied from SOURCE, or the default-face
-if SOURCE is not defined.
+a new one, its initial state will be to have absolutely no effect.
 ::end:: */
 {
     Lisp_Face *f;
-    int id;
 
     DECLARE1(name, STRINGP);
 
@@ -81,51 +84,16 @@ if SOURCE is not defined.
 	    return VAL(f);
     }
 
-    if(!FACEP(src))
-	src = cmd_symbol_value(sym_default_face, sym_t);
-
-    for(id = GA_FirstFace; id <= GA_LastFace && face_table[id] != 0; id++) ;
-    if(id >= GA_LastFace)
-	return mem_error();		/* !? */
-
     f = ALLOC_OBJECT(sizeof(Lisp_Face));
     if(f == 0)
 	return mem_error();
     f->next = allocated_faces;
     allocated_faces = f;
-    f->id = id;
     f->name = name;
+    f->car = V_Face;
+    f->foreground = f->background = sym_nil;
 
-    if(FACEP(src))
-    {
-	f->car = VFACE(src)->car;
-	f->foreground = VFACE(src)->foreground;
-	f->background = VFACE(src)->background;
-    }
-    else
-    {
-	f->car = V_Face;
-	f->foreground = f->background = sym_nil;
-    }
-
-    face_table[id] = f;
     return VAL(f);
-}
-
-_PR VALUE cmd_delete_face(VALUE);
-DEFUN("delete-face", cmd_delete_face, subr_delete_face, (VALUE face),
-      V_Subr1, DOC_delete_face) /*
-::doc:delete_face::
-delete-face FACE
-
-Mark that FACE's slot in the table of faces may be reused. FACE will no
-longer be displayable.
-::end:: */
-{
-    DECLARE1(face, FACEP);
-    face_table[VFACE(face)->id] = 0;
-    VFACE(face)->id = 0;
-    return face;
 }
 
 void
@@ -161,17 +129,6 @@ face_sweep(void)
     }
 }
 
-void
-mark_faces(void)
-{
-    int i;
-    for(i = GA_FirstFace; i <= GA_LastFace; i++)
-    {
-	if(face_table[i] != 0)
-	    MARKVAL(VAL(face_table[i]));
-    }
-}
-
 _PR VALUE cmd_set_face_attribute(VALUE, VALUE, VALUE);
 DEFUN("set-face-attribute", cmd_set_face_attribute, subr_set_face_attribute,
       (VALUE face, VALUE attr, VALUE value), V_Subr3,
@@ -187,7 +144,7 @@ may be one of these symbols:
 	that VALUE may be either the color object, or the name of the
 	color.
 
-  `underline', 'bold', `italic'
+  `underline', 'bold', `italic', `inverted', `boxed'
 	Boolean attributes.
 ::end:: */
 {
@@ -202,7 +159,8 @@ may be one of these symbols:
 	    if(!value)
 		return value;
 	}
-	DECLARE3(value, COLORP);
+	if(!NILP(value))
+	    DECLARE3(value, COLORP);
 	if(attr == sym_foreground)
 	    VFACE(face)->foreground = value;
 	else
@@ -226,32 +184,26 @@ may be one of these symbols:
 	if(!NILP(value))
 	    VFACE(face)->car |= FACEFF_ITALIC;
     }
+    else if(attr == sym_inverted)
+    {
+	VFACE(face)->car &= ~FACEFF_INVERT;
+	if(!NILP(value))
+	    VFACE(face)->car |= FACEFF_INVERT;
+    }
+    else if(attr == sym_boxed)
+    {
+	VFACE(face)->car &= ~FACEFF_BOXED;
+	if(!NILP(value))
+	    VFACE(face)->car |= FACEFF_BOXED;
+    }
     else
 	return signal_arg_error(attr, 2);
 
     for(w = win_chain; w != 0; w = w->w_Next)
     {
 	if(w->w_Window)
-	{
-	    int row, fid = VFACE(face)->id;
-	    glyph_buf *g = w->w_Content;
-	    for(row = 0; row < g->rows; row++)
-	    {
-		glyph_attr *attrs = g->attrs[row];
-		bool modified = FALSE;
-		int col;
-		for(col = 0; col < g->cols; col++)
-		{
-		    if((attrs[col] & ~GA_CursorFace) == fid)
-		    {
-			attrs[col] = GA_Garbage;
-			modified = TRUE;
-		    }
-		}
-		if(modified)
-		    g->hashes[row] = hash_glyph_row(g, row);
-	    }
-	}
+	    /* TODO: this is highly suboptimal... */
+	    w->w_Flags |= WINFF_FORCE_REFRESH;
     }
 
     return value;
@@ -279,23 +231,139 @@ See `set-face-attribute' for the list of possible attributes.
 	return (VFACE(face)->car & FACEFF_BOLD) ? sym_t : sym_nil;
     else if(attr == sym_italic)
 	return (VFACE(face)->car & FACEFF_ITALIC) ? sym_t : sym_nil;
+    else if(attr == sym_inverted)
+	return (VFACE(face)->car & FACEFF_INVERT) ? sym_t : sym_nil;
+    else if(attr == sym_boxed)
+	return (VFACE(face)->car & FACEFF_BOXED) ? sym_t : sym_nil;
     else
 	return signal_arg_error(attr, 2);
 }
 
-_PR VALUE cmd_face_id(VALUE);
-DEFUN("face-id", cmd_face_id, subr_face_id, (VALUE face),
-      V_Subr1, DOC_face_id) /*
-::doc:face_id::
-face-id FACE
+
+/* rendering with faces */
 
-Return the id of the slot in the face table used by FACE. Returns nil
-if this face has been deleted but not garbage collected.
-::end:: */
+static int
+get_merged_face(WIN *w, u_short car,
+		Lisp_Color *background, Lisp_Color *foreground)
 {
-    DECLARE1(face, FACEP);
+    int id, empty = -1;
 
-    return VFACE(face)->id != 0 ? MAKE_INT(VFACE(face)->id) : sym_nil;
+    /* Find a space in the MergedFaces table, either containing
+       an equivalent of this one, or an empty slot. */
+    for(id = GA_FirstFace; id <= GA_LastFace; id++)
+    {
+	if(w->w_MergedFaces[id].valid)
+	{
+	    Merged_Face *f = &w->w_MergedFaces[id];
+	    if(f->car == car
+	       && f->background == background
+	       && f->foreground == foreground)
+	    {
+		return id;
+	    }
+	}
+	else if(empty == -1)
+	    empty = id;
+    }
+    if(empty == -1)
+	return GA_FirstFace;	/* lame */
+
+    /* Fill the new face */
+    w->w_MergedFaces[empty].car = car;
+    w->w_MergedFaces[empty].valid = TRUE;
+    w->w_MergedFaces[empty].background = background;
+    w->w_MergedFaces[empty].foreground = foreground;
+    return empty;
+}
+
+/* Return the id of a face in W->w_MergedFaces that expresses the
+   attributes of the positions within E. */
+int
+merge_faces(WIN *w, Lisp_Extent *e, int in_block, int on_cursor)
+{
+    u_long car = 0;
+    Lisp_Color *background = 0, *foreground = 0;
+
+    Lisp_Extent *x;
+
+    void union_face(Lisp_Face *face) {
+	car |= face->car & (FACEFF_MASK & ~FACEFF_INVERT);
+	if(face->car & FACEFF_INVERT)
+	    car ^= FACEFF_INVERT;
+	if(background == 0 && COLORP(face->background))
+	    background = VCOLOR(face->background);
+	if(foreground == 0 && COLORP(face->foreground))
+	    foreground = VCOLOR(face->foreground);
+    }
+
+    if(on_cursor)
+    {
+	if(WINDOW_HAS_FOCUS(w))
+	    car |= FACEFF_INVERT;
+	else
+	    car |= FACEFF_BOXED;
+    }
+
+    if(in_block)
+    {
+	VALUE face = VSYM(sym_block_face)->value;
+	if(FACEP(face))
+	    union_face(VFACE(face));
+    }
+
+    /* Work up from E to the root. */
+    for(x = e; x != 0; x = x->parent)
+    {
+	VALUE face = cmd_extent_get(VAL(x), sym_face);
+	if(FACEP(face))
+	    union_face(VFACE(face));
+    }
+
+    /* Merge in the default-face properties */
+    {
+	VALUE face = VSYM(sym_default_face)->value;
+	if(FACEP(face))
+	    union_face(VFACE(face));
+    }
+
+    assert(background != 0 && foreground != 0);
+
+    return get_merged_face(w, car, background, foreground);
+}
+
+int
+get_face_id(WIN *w, Lisp_Face *f)
+{
+    assert(COLORP(f->background) && COLORP(f->foreground));
+    return get_merged_face(w, f->car & FACEFF_MASK,
+			   VCOLOR(f->background), VCOLOR(f->foreground));
+}
+
+void
+mark_merged_faces(WIN *w)
+{
+    glyph_buf *g = w->w_Content;
+    int row, col;
+    int id;
+
+    for(id = GA_FirstFace; id <= GA_LastFace; id++)
+	w->w_MergedFaces[id].valid = FALSE;
+
+    for(row = 0; row < g->rows; row++)
+    {
+	glyph_attr *attrs = g->attrs[row];
+	for(col = 0; col < g->cols; col++)
+	    w->w_MergedFaces[attrs[col]].valid = TRUE;
+    }
+
+    for(id = GA_FirstFace; id <= GA_LastFace; id++)
+    {
+	if(w->w_MergedFaces[id].valid)
+	{
+	    MARKVAL(VAL(w->w_MergedFaces[id].background));
+	    MARKVAL(VAL(w->w_MergedFaces[id].foreground));
+	}
+    }
 }
 
 
@@ -377,10 +445,8 @@ faces_init(void)
     VALUE face, fg, bg, bl, hl, ml;
 
     ADD_SUBR(subr_make_face);
-    ADD_SUBR(subr_delete_face);
     ADD_SUBR(subr_set_face_attribute);
     ADD_SUBR(subr_face_attribute);
-    ADD_SUBR(subr_face_id);
     ADD_SUBR(subr_get_color);
 
     INTERN(foreground);
@@ -388,6 +454,8 @@ faces_init(void)
     INTERN(underline);
     INTERN(bold);
     INTERN(italic);
+    INTERN(inverted);
+    INTERN(boxed);
     INTERN(default_face);
     INTERN(block_face);
     INTERN(modeline_face);
@@ -400,23 +468,21 @@ faces_init(void)
     hl = cmd_get_color(string_dup(default_hl_color));
     ml = cmd_get_color(string_dup(default_ml_color));
 
-    face = cmd_make_face(VSYM(sym_default_face)->name, sym_nil);
+    face = cmd_make_face(VSYM(sym_default_face)->name);
     cmd_set_face_attribute(face, sym_foreground, fg);
     cmd_set_face_attribute(face, sym_background, bg);
     VSYM(sym_default_face)->value = face;
 
-    face = cmd_make_face(VSYM(sym_block_face)->name, sym_nil);
-    cmd_set_face_attribute(face, sym_foreground, fg);
+    face = cmd_make_face(VSYM(sym_block_face)->name);
     cmd_set_face_attribute(face, sym_background, bl);
     VSYM(sym_block_face)->value = face;
 
-    face = cmd_make_face(VSYM(sym_modeline_face)->name, sym_nil);
+    face = cmd_make_face(VSYM(sym_modeline_face)->name);
     cmd_set_face_attribute(face, sym_foreground, fg);
     cmd_set_face_attribute(face, sym_background, ml);
     VSYM(sym_modeline_face)->value = face;
 
-    face = cmd_make_face(VSYM(sym_highlight_face)->name, sym_nil);
-    cmd_set_face_attribute(face, sym_foreground, fg);
+    face = cmd_make_face(VSYM(sym_highlight_face)->name);
     cmd_set_face_attribute(face, sym_background, hl);
     VSYM(sym_highlight_face)->value = face;
 }
