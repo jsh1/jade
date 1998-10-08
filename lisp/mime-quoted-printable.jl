e69f375be25c1c28e23bb20376b8cbcd1918b36b
john <john>
1998-10-08 19:19:25 +0000

    initial support for decoding of MIME messages. The quoted-printable stuff is unimplemented

---
+++ lisp/mime-quoted-printable.jl
@@ -0,0 +1,27 @@
+;;;; mime-quoted-printable.jl -- Encoder/decoder for quoted-printable encoding
+;;;  Copyright (C) 1998 John Harper <john@dcs.warwick.ac.uk>
+;;;  $Id$
+
+;;; This file is part of Jade.
+
+;;; Jade is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+
+;;; Jade is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+
+;;; You should have received a copy of the GNU General Public License
+;;; along with Jade; see the file COPYING.  If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+(provide 'mime-quoted-printable)
+
+(defun mime-encode-quoted-printable (input output)
+  (error "quoted-printable encoding unimplemented"))
+
+(defun mime-decode-quoted-printable (input output)
+  (error "quoted-printable decoding unimplemented"))
