a7a5d83d4c39b4fea9b0adc8799d566a7ae62109
john <john>
1999-03-27 20:53:35 +0000

    (mark_glyph_buf_faces): be more careful in which attributes are taken as valid

--- src/faces.c
+++ src/faces.c
@@ -357,8 +357,11 @@ mark_glyph_buf_faces(WIN *w, glyph_buf *g)
 	glyph_attr *attrs = g->attrs[row];
 	for(col = 0; col < g->cols; col++)
 	{
-	    if(attrs[col] != GA_Garbage)
+	    if(attrs[col] <= GA_LastFace)
 		w->w_MergedFaces[attrs[col]].valid = TRUE;
+	    else if (attrs[col] != GA_Garbage)
+		fprintf (stderr, "warning: invalid glyph attr (%d,%d) = %d\n",
+			 row, col, attrs[col]);
 	}
     }
 
