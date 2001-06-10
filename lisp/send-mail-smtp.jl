b7b67ee4e7055edbb385a50c7758a2c7c33bccae
jsh <jsh>
2001-06-10 05:33:14 +0000

    2001-06-09  John Harper  <jsh@pixelslut.com>
    	* send-mail-smtp.jl: minor changes; added some comments

--- lisp/send-mail-smtp.jl
+++ lisp/send-mail-smtp.jl
@@ -20,15 +20,22 @@
 ;;; along with Jade; see the file COPYING.  If not, write to
 ;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
+(require 'maildefs)
 (provide 'send-mail-smtp)
 
-(defvar mail-smtp-server nil)
-(defvar mail-smtp-port 25)
+(defvar mail-smtp-server nil
+  "The name of the host to use as an SMTP server.")
+
+(defvar mail-smtp-port 25
+  "The port number of the SMTP server. Normally port 25.")
+
+(defvar mail-smtp-return-address user-mail-address
+  "The mail address used as the return address when sending messages via
+SMTP directly.")
 
 ;; put this in the send-mail-send-it-function variable
 (define (smtp-send-it normal-addresses resent-addresses temp-buffer)
 
-  (require 'maildefs)
   (require 'mail-headers)
   (require 'rep.io.sockets)
 
@@ -40,6 +47,7 @@
 	(error-occurred nil)
 	socket)
 
+    ;; Receives output from the SMTP server. Looks for result strings
     (define (smtp-output output)
       (write temp-buffer output)
       (when pending-output
@@ -51,6 +59,8 @@
 				  (expand-last-match "\\2")))
 	(setq pending-output (substring output (match-end)))))
 
+    ;; Output a single SMTP command; waits for the result before returning
+    ;; Signals an error unless the SMTP command succeeds
     (define (smtp-command fmt . args)
       (apply format socket fmt args)
       (setq got-result nil)
@@ -60,52 +70,63 @@
 	(when error-occurred
 	  (error "SMTP error: %s" error-occurred))))
 
+    ;; Send the list of unparsed address strings ADDRESSES as "RCPT To:"
+    ;; SMTP commands. Signals an error if any fail
     (define (send-rcpts addresses)
       (mapc (lambda (x)
 	      (let ((addr (mail-parse-address x)))
 		(when (car addr)
+		  ;; XXX until I get confidence in this code,
+		  ;; XXX print all addresses sent to
 		  (format standard-error "sent: %s\n" (car addr))
 		  (smtp-command "RCPT To: <%s>\n" (car addr)))))
 	    addresses))
 
+    ;; Create the socket used to talk to the SMTP server
     (setq socket (socket-client mail-smtp-server mail-smtp-port smtp-output))
 
+    ;; Log on
     (smtp-command "HELO %s\n" (system-name))
 
-    (smtp-command "MAIL From: <%s>\n" user-mail-address)
+    ;; Start a mail message by sending our return address
+    (smtp-command "MAIL From: <%s>\n" mail-smtp-return-address)
 
+    ;; Send the recipient addresses
     (send-rcpts normal-addresses)
     (send-rcpts resent-addresses)
 
+    ;; Then send the message itself (including headers). Quote `.' at the
+    ;; start of a line as `..', as per the SMTP standard
     (smtp-command "DATA\n")
     (let ((point (start-of-buffer))
 	  (seen-date nil)
 	  (seen-message-id)
 	  (in-header t))
       (while (< point (end-of-buffer))
-	(let* ((next (forward-line 1 point))
-	       (line (copy-area point next)))
-	  (when in-header
-	    (cond ((looking-at "Date[ \t]*:" point nil t)
-		   (setq seen-date t))
-		  ((looking-at "Message-ID[ \t]*:" point nil t)
-		   (setq seen-message-id t))
-		  ((looking-at "^[ \t]*$" point)
-		   (setq in-header nil)
-		   (unless seen-date
-		     (format socket "Date: %s\n"
-			     (current-time-string
-			      nil "%a, %d %b %Y %H:%M:%S %z")))
-		   (unless seen-message-id
-		     (format socket "Message-ID: %s\n" (make-message-id))))))
-	  (if (and in-header (looking-at "^(Resent-)BCC[ \t]*:" point nil t))
-	      ;; skip bcc headers
-	      (setq point (mail-unfold-header point))
+	(when in-header
+	  (cond ((looking-at "Date[ \t]*:" point nil t)
+		 (setq seen-date t))
+		((looking-at "Message-ID[ \t]*:" point nil t)
+		 (setq seen-message-id t))
+		((looking-at "^[ \t]*$" point)
+		 (setq in-header nil)
+		 (unless seen-date
+		   (format socket "Date: %s\n"
+			   (current-time-string
+			    nil "%a, %d %b %Y %H:%M:%S %z")))
+		 (unless seen-message-id
+		   (format socket "Message-ID: %s\n" (make-message-id))))))
+	(if (and in-header (looking-at "^(Resent-)BCC[ \t]*:" point nil t))
+	    ;; skip bcc headers
+	    (setq point (mail-unfold-header point))
+	  (let* ((next (forward-line 1 point))
+		 (line (copy-area point next)))
 	    (when (eql (aref line 0) #\.)
 	      (write socket #\.))
 	    (write socket line)
 	    (setq point next)))))
     (smtp-command ".\n")
 
+    ;; Finished, log off and shutdown the connection
     (smtp-command "QUIT\n")
     (close-socket socket)))
